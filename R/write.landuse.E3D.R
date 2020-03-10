5c7f2d456fc16eda904cffd7f3535cbecc96921f
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2020-03-10 15:10:11 +0100

    bug fix in write.lulc.E3D: filename was not used

--- R/write.landuse.E3D.R
+++ R/write.landuse.E3D.R
@@ -8,9 +8,9 @@
 #' @seealso
 #' @export
 #' @examples
-#' write.lulc.E3D(path = "C:/")
+#' write.landuse.E3D(path = "C:/")
 
-write.lulc.E3D <- function(POLY_ID = c(1,2,3),length=50, path, filename = "lulc.asc")
+write.landuse.E3D <- function(POLY_ID = c(1,2,3),length=50, path, filename = "/model/soil/landuse.asc")
 {
 require(raster)
     #EROSION-3D requires at least two rows
@@ -19,7 +19,7 @@ require(raster)
   rows <- length(POLY_ID)
 writeRaster(
   raster(nrows=rows, ncols=length, xmn=0, xmx=length, ymn=0, ymx=rows,crs="+notaCRS", vals=rep(POLY_ID, each=length)),
-  filename = paste0(path,"lulc.asc"),
+  filename = paste0(path,filename),
   format ="ascii",
   overwrite=TRUE,
   datatype = "INT2U"
