c056077281516c4f0a257e26730fcd7bc25002f2
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2020-03-16 16:14:40 +0100

    roxygenization of: - write.landuse - write.relief

--- R/write.landuse.E3D.R
+++ R/write.landuse.E3D.R
@@ -1,22 +1,26 @@
 #' Write an synthetic EROSION-3D landclassification input file
 #'
 #' This function writes a landuse/landcover file for simultaneously modelling of EROSION-2D slopes in EROSION-3D.
-#' One row represents one homogeneous soil parameter set.
-#' Length defines slope length.
-#' @inheritParams
-#' @return No return value, but file is written
-#' @seealso
+#' One row represents one EROSION-2D slope, when flow routing is set to one neighbour in EROSION-3D.
+#'
+#' @param POLY_ID numeric vector, number of elements defines columns of created relief file. Refers to POLY_ID in soil_params.csv of E3D.
+#' @param length numeric value, defining slope length in whole meters
+#' @param path folder path in which file will be written
+#' @param filename name of created file
+#' @return None, file is written
 #' @export
+#' @importFrom raster writeRaster
+#' @importFrom raster raster
 #' @examples
-#' write.landuse.E3D(path = "C:/")
+#' write.landuse.E3D(path = "C:/E3Dmodel/")
 
 write.landuse.E3D <- function(POLY_ID = c(1,2,3),length=50, path, filename = "/model/soil/landuse.asc")
 {
-require(raster)
     #EROSION-3D requires at least two rows
   if (length(POLY_ID) < 2){POLY_ID <- rep(POLY_ID, each=2);}
 
   rows <- length(POLY_ID)
+
 writeRaster(
   raster(nrows=rows, ncols=length, xmn=0, xmx=length, ymn=0, ymx=rows,crs="+notaCRS", vals=rep(POLY_ID, each=length)),
   filename = paste0(path,filename),
--- R/write.relief.E3D.R
+++ R/write.relief.E3D.R
@@ -1,19 +1,22 @@
 #' Write an synthetic EROSION-3D relief input file
 #'
-#' This function writes a relief file for simultaneously modelling of EROSION-2D slopes in EROSION-3D.
-#' One row represents one EROSION-2D slope.
-#' Length defines slope length.
-#' Slope is given in %.
-#' @inheritParams
-#' @return No return value, but file is written
-#' @seealso
+#' This function writes a relief file for simultaneously modelling of independent hillslopes (EROSION-2D) in EROSION-3D.
+#' One row represents one EROSION-2D slope, when flow routing is set to one neighbour in EROSION-3D.
+#'
+#' @param POLY_ID numeric vector, number of elements defines columns of created relief file. Refers to POLY_ID in soil_params.csv of E3D.
+#' @param length numeric value, defining slope length in whole meters
+#' @param slope numeric value, defining slope inclination in whole percent
+#' @param path folder path in which file will be written
+#' @param filename name of created file
+#' @return None, file is written
+#' @importFrom raster writeRaster
+#' @importFrom raster raster
 #' @export
 #' @examples
-#' write.relief.E3D(path = "C:/")
+#' write.relief.E3D(POLY_ID = c(1,2,3),length = 50,slope = 11, path = "C:/E3Dmodel/", filename = "dem.asc")
 
-write.relief.E3D <- function(POLY_ID = c(1,2,3),length=50,slope = 11, path, filename = "dem.asc")
+write.relief.E3D <- function(POLY_ID, length, slope, path, filename = "dem.asc")
 {
-require(raster)
   #EROSION-3D requires at least two rows
   if (length(POLY_ID) < 2){POLY_ID <- rep(POLY_ID, each=2);}
 
@@ -21,7 +24,7 @@ require(raster)
 
 writeRaster(
   raster(nrows=rows, ncols=length, xmn=0, xmx=length, ymn=0, ymx=rows,crs="+notaCRS", vals=rep((1:length)*slope/100, times=rows)),
-  filename = paste0(path,"dem.asc"),
+  filename = paste0(path,filename),
   format ="ascii",
   overwrite=TRUE
 )
