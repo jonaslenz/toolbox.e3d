622c286f1b1171572d06385cff5feefa613da173
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2020-03-19 10:35:55 +0100

    add documentation to determine.skin.runoff.E3D

--- R/determine.skin.runoff.E3D.R
+++ R/determine.skin.runoff.E3D.R
@@ -1,11 +1,21 @@
 #' Skinfactor determination
 #'
-#' This function models a rainfall-experiment plot in EROSION-3D.
-#' The calibration parameter skinfactor is determined that calculated cummulative runoff equals the measured value.
+#' This function calibrates parameter skinfactor that calculated cummulative runoff equals the measured value from a rainfall experiment.
+#' Therfore it models a rainfall-experiment plot using EROSION-3D.
 #'
+#' @param CumRunoff numeric value, cummulative runoff is the fitting target of this function
+#' @param intensity numeric value, a constant rainfall intensity during the experiment
+#' @param plotwidth numeric value, width of the experimental plot, CumRunoff will be normalized to one meter width using this parameter
+#' @param plotlength integer value, length of experimental plot, needs to be an interger due to spatial resolution of 1 meter in E3D
+#' @param slope integer value, mean slope of experimental plot in percent
+#' @param endmin integer value, duration of rainfall experiment in full minutes rainfall was applied.
+#' @param ponding logical TRUE means ponding option is used, FALSE - is not used in E3D, ponding limits amount of infiltrating water to available water
+#' @param simlines integer value, number of parallel computated plots, higher numbers decrease number of iteration steps with E3D, but increases number of write-read operations
+#' @param path path to modelling directory, defaul is a temporary directory
+#' @param silent logical, if TRUE skinfactor iteration steps will be written as message
 #' @importFrom raster raster
 #' @export
-#' @examples
+#' @examples determine.skin.runoff.E3D(Cl = 30, Si = 40, Sa = 30, Corg = 1.3, Bulk = 1300, Moist = 22, CumRunoff = 100, intensity = 0.5, plotwidth = 1, plotlength = 10, slope = 10, endmin = 30, ponding = TRUE, silent = FALSE)
 #'
 determine.skin.runoff.E3D <- function(Cl, Si, Sa, Corg, Bulk, Moist, CumRunoff, intensity, plotwidth, plotlength, slope, endmin, ponding =FALSE, simlines = 100, path = tempdir(), silent=TRUE)
 {
