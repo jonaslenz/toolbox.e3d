fc4c038e1c6e9355f13f1d9b598974c47fe49378
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2020-06-08 00:55:16 +0200

    add get version function

---
+++ R/get_version.E3D.R
@@ -0,0 +1,16 @@
+#' reads version of E3D installation
+#'
+#' Returns the version number of the E3D installation given in the relief.log file in relief set created by E3D.
+#' @return string E3D version number
+#' @export
+#' @importFrom ini read.ini
+#' @examples
+#' get_version.E3D()
+
+get_version.E3D <- function()
+{
+  path = create_folders.E3D(path = paste0(tempdir(),"\\version"), overwrite = TRUE)
+  system2("e3d", paste0('/r "',normalizePath(file.path(path,"run.par")),'"'), wait=TRUE)
+  version <- ini::read.ini(normalizePath(file.path(path,"relief/relief.log")))$Relief$Version
+  return(version);
+}
