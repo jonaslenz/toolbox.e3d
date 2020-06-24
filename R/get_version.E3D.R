cb0e3eed322b80e77bea1285052fa6c093fb4b89
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2020-06-24 10:25:58 +0200

    option to get/not get build information of e3d in get_version

--- R/get_version.E3D.R
+++ R/get_version.E3D.R
@@ -2,16 +2,22 @@
 #'
 #' Returns the version number of the E3D installation given in the relief.log file in relief set created by E3D.
 #' @return string E3D version number
+#' @param build logical, TRUE returns a string including build information
 #' @export
 #' @importFrom ini read.ini
 #' @examples
 #' get_version.E3D()
 
-get_version.E3D <- function()
+get_version.E3D <- function(build = FALSE)
 {
   if(check_setup.E3D()!=0){stop("cannot access e3d instalation")}
   path = create_folders.E3D(path = paste0(tempdir(),"\\version"), overwrite = TRUE)
   system2("e3d", paste0('/r "',normalizePath(file.path(path,"run.par")),'"'), wait=TRUE)
   version <- ini::read.ini(normalizePath(file.path(path,"relief/relief.log")))$Relief$Version
+
+  if(!build)
+  {
+    version <- gsub(" .*","",version)
+  }
   return(version);
 }
