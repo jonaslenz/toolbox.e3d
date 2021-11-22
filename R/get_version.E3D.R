004d34a309bb36acdab55eb094e72611ad7f6b5c
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2021-11-22 11:11:34 +0100

    possibility to add custom installation path; export check_setup

--- R/get_version.E3D.R
+++ R/get_version.E3D.R
@@ -3,14 +3,15 @@
 #' Returns the version number of the E3D installation given in the relief.log file in relief set created by E3D.
 #' @return string E3D version number
 #' @param build logical, TRUE returns a string including build information
+#' @param install_path, path to installation directory of EROSION-3D
 #' @export
 #' @importFrom ini read.ini
 #' @examples
 #' get_version.E3D()
 
-get_version.E3D <- function(build = FALSE)
+get_version.E3D <- function(build = FALSE, install_path = NA)
 {
-  if(check_setup.E3D()!=0){stop("cannot access e3d instalation")}
+  if(check_setup.E3D(install_path = install_path)!=0){stop("cannot access e3d instalation")}
   path = create_folders.E3D(path = paste0(tempdir(),"\\version"), overwrite = TRUE)
   system2("e3d", paste0('/r "',normalizePath(file.path(path,"run.par")),'"'), wait=TRUE)
   version <- ini::read.ini(normalizePath(file.path(path,"relief/relief.log")))$Relief$Version
